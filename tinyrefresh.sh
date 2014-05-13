47eba13505b0d52d0815cbf53727abaefcccd764
Neel Smith <dnsmith.neel@gmail.com>
2014-05-13 14:53:33 -0400

    Add short bash script for testing in different environments.

---
+++ tinyrefresh.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+echo Starting the latinsources servlet
+GIT=`which git`
+GRADLE=`which gradle`
+echo Will use git from $GIT
+echo Will use gradle from $GRADLE
+# What we need:
+# 1. the latinsrcs archive.
+# 2. citemgr
+# 3. citeservlet
+# Phoros archive:
+if [ -d "/vagrant/latinsrcs" ]
+then
+    echo "Checking archive for updates"
+    cd /vagrant/latinsrcs
+    $GIT pull
+else
+    echo "Installing phoros archive"
+    cd /vagrant
+    echo  Running  $GIT clone https://github.com/neelsmith/latinsrcs
+    $GIT clone https://github.com/neelsmith/latinsrcs
+fi
